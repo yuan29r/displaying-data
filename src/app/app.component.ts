c78199846e48cc606ac8f817f05e42c73affafa1
yuan29r <sheng29r@gmail.com>
2020-01-03 16:26:47 +0800

    constructor 宣告欄位，然後import class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 
+// import class
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero}}</h2>
+  <h2>My favorite hero is: {{myHero.name}}</h2>
+  <p>Heroes: </p>
+    <ul>
+      <li *ngFor="let hero of heroes">{{hero.name}}</li>
+    </ul>
+    <p *ngIf="heroes.length >3 ">There are many heroes!</p>
    `
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  myHero = 'Windstorm';
+  // myHero = 'Windstorm';
+  // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
 }
